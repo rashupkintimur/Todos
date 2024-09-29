040f6ebeb93a17011187e21d3bc5339c20a39699
xqdt <oxymususer@gmail.com>
2024-09-29 11:22:47 +0500

    added color in switch language

--- src/ui/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/ui/LanguageSwitcher/LanguageSwitcher.tsx
@@ -15,13 +15,13 @@ export const LanguageSwitcher: FC<LanguageSwitcherProps> = () => {
     <div className="flex items-center gap-5">
       <button
         onClick={() => changeLanguage("en")}
-        className="text-white text-xl hover:text-red-500 active:text-red-700 focus:outline-none focus:ring focus:ring-red-500 duration-150"
+        className="text-slate-800 dark:text-white text-xl hover:text-red-500 active:text-red-700 focus:outline-none focus:ring focus:ring-red-500 duration-150"
       >
         {t("english")}
       </button>
       <button
         onClick={() => changeLanguage("ru")}
-        className="text-white text-xl hover:text-red-500 active:text-red-700 focus:outline-none focus:ring focus:ring-red-500 duration-150"
+        className="text-slate-800 dark:text-white text-xl hover:text-red-500 active:text-red-700 focus:outline-none focus:ring focus:ring-red-500 duration-150"
       >
         {t("russian")}
       </button>
